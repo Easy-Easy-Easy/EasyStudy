f3d508e0f7abd34eefd0617299eeb31ccf12f0c0
duo.zheng <zhengduo@jixianyuan.com>
2019-04-17 15:27:14 +0800

    增加动态代理实现的方式

--- Proxy/src/main/java/com/easy/test.java
+++ Proxy/src/main/java/com/easy/test.java
@@ -22,5 +22,9 @@ public class test {
         DynamicProxy dynamicProxy = new DynamicProxy(shopping);
         Shopping proxyShopping=(Shopping) dynamicProxy.getObject();
         proxyShopping.buyWawa("王二狗");
+        //动态代理的实现原理
+        //在调用Proxy.newProxyInstance 方法时，会生成一个名为$Proxy0的类,该类继承了Porxy,实现了传入的接口
+        //在每一个方法的调用处,都会调用this.h.invoke 方法,而h对象则为传入的实现了InvocationHandler 的类(DynamicProxy)
+        //调用该对象的invoke 方法,就实现了动态代理的整个过程
     }
 }
